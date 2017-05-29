7c8e1e25916c31e4d4ef6b9446766cbe503a0600
Daniel Oaks <daniel@danieloaks.net>
2017-05-28 23:48:05 -0600

    Rename documents to match I-D naming conventions

--- build-ctcp.sh
+++ build-ctcp.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 # make xml
-kramdown-rfc2629 id-oakley-irc-ctcp-latest.mkd > dist/id-oakley-irc-ctcp-latest.xml
+kramdown-rfc2629 draft-oakley-irc-ctcp-latest.mkd > dist/draft-oakley-irc-ctcp-latest.xml
 
 # make txt
-xml2rfc dist/id-oakley-irc-ctcp-latest.xml -o dist/id-oakley-irc-ctcp-latest.txt --text
+xml2rfc dist/draft-oakley-irc-ctcp-latest.xml -o dist/draft-oakley-irc-ctcp-latest.txt --text
 
 # make html
-xml2rfc dist/id-oakley-irc-ctcp-latest.xml -o dist/draft-tmp.html --html
+xml2rfc dist/draft-oakley-irc-ctcp-latest.xml -o dist/draft-tmp.html --html
 
-cat dist/draft-tmp.html | sed -f lib/addstyle.sed > dist/id-oakley-irc-ctcp-latest.html
+cat dist/draft-tmp.html | sed -f lib/addstyle.sed > dist/draft-oakley-irc-ctcp-latest.html
 rm dist/draft-tmp.html
